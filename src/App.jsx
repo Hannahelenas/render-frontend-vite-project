c4f043e284a283b4b0e7f20d26e2c97c849c404f
Hannahelenas <hanna.eriksson@iths.se>
2024-05-07 15:59:16 +0200

    Testar ändringar med Webhooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -18,11 +17,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Hanna testar Render! 
+          Hanna testar Render!
         </p>
       </div>
       <p className="read-the-docs">
